5cca72603dfbadd6b04d5b8fcd8985a68fe76475
Ayoub <ayoubsaada10@gmail.com>
2025-05-16 14:36:12 +0000

    Agregar soporte para CORS en la aplicación; configurar opciones de CORS para permitir solicitudes desde el origen especificado y ajustar el controlador de la aplicación para incluir la configuración de CORS.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,15 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { AppRoutes } from "./app.routes";
+import cors from "@elysiajs/cors";
 
-new Elysia().use(swagger()).use(AppRoutes).listen(8888);
+new Elysia().use(
+  cors({
+    origin: 'https://chat.asaada10.dev',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id'],
+    maxAge: 86400
+  })
+).options('*', () => '').use(swagger()).use(AppRoutes).listen(8888);
 console.log("🚀 Elysia app running at https://localhost:8888");
